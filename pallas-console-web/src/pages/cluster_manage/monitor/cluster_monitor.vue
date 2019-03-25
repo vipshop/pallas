d97e19d58496eb8187a3062efa3dbcd984dd4274
gier.cai <gier.cai@vipshop.com>
2019-03-25 16:54:46 +0800

    #30 增加集群监控总览

--- pallas-console-web/src/pages/cluster_manage/monitor/cluster_monitor.vue
+++ pallas-console-web/src/pages/cluster_manage/monitor/cluster_monitor.vue
@@ -1,5 +1,22 @@
 <template>
     <div class="my-tab-content" v-loading="loading" element-loading-text="请稍等···">
+        <div class="monitor-top">
+            <el-table :data="gaugeMetricData" border style="width: 100%">
+                <el-table-column prop="nodeCount" label="Nodes"></el-table-column>
+                <el-table-column prop="indexCount" label="Indices"></el-table-column>
+                <el-table-column label="Memory">
+                  <template scope="scope">
+                      {{scope.row.used_memory_byte}} / {{scope.row.total_memory_byte}}
+                  </template>
+                </el-table-column>
+                <el-table-column prop="totalShardCount" label="Total Shards"></el-table-column>
+                <el-table-column prop="unassignedShardCount" label="Unassigned Shards"></el-table-column>
+                <el-table-column prop="documentCount" label="Documents"></el-table-column>
+                <el-table-column prop="document_store_byte" label="Data"></el-table-column>
+                <el-table-column prop="version" label="Version"></el-table-column>
+                <el-table-column prop="health" label="Health"></el-table-column>
+            </el-table>
+        </div>
         <div>
             <el-row :gutter="10">
                 <el-col :xs="24" :sm="24" :md="24" :lg="24" class="chart-auto-size">
@@ -18,7 +35,7 @@ export default {
   data() {
     return {
       loading: false,
-      searchRateInfo: {},
+      gaugeMetricData: [],
       indexingRateInfo: {},
     };
   },
@@ -41,6 +58,7 @@ export default {
       };
       this.$http.post('/monitor/cluster.json', params).then((data) => {
         if (data) {
+          this.gaugeMetricData.push(data.gaugeMetric);
           this.getIndexingRate(data.indexingRate);
         }
       });
@@ -65,3 +83,15 @@ export default {
   },
 };
 </script>
+<style>
+.monitor-top {
+  margin-bottom: 10px;
+}
+.monitor-top .el-table .cell, .monitor-top .el-table th>div{
+  padding-top: 0px;
+  padding-bottom: 0px;
+}
+.monitor-top .el-table td, .monitor-top .el-table th.is-leaf {
+    border-bottom: none;
+}
+</style>
