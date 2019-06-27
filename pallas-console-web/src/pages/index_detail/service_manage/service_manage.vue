d533621caa6554368d4726ad08a8cee78bf8201e
gier.cai <gier.cai@vipshop.com>
2019-06-27 17:55:32 +0800

    #81 platform/pallas#860 模板限流，超时重试页面代码整改及优化

--- pallas-console-web/src/pages/index_detail/service_manage/service_manage.vue
+++ pallas-console-web/src/pages/index_detail/service_manage/service_manage.vue
@@ -1,29 +1,46 @@
 <template>
     <div class="my-tab-content">
-        <el-tabs v-model="tabActive">
+        <el-tabs v-model="tabActive" v-loading="loading" element-loading-text="请稍等···">
             <el-tab-pane label="超时重试" name="timeout_retry">
-                <timeout-retry-manage></timeout-retry-manage>
+                <timeout-retry-manage :template-list="templateList" @refresh-template="getTemplateList"></timeout-retry-manage>
             </el-tab-pane>
             <el-tab-pane label="限流配置" name="throttling">
-                <throttling-manage></throttling-manage>
+                <throttling-manage :template-list="templateList" @refresh-template="getTemplateList"></throttling-manage>
             </el-tab-pane>
         </el-tabs>
     </div>
 </template>
 
 <script>
-    import TimeoutRetryManage from './timeout_retry_dialog/timeout_retry_manage';
-    import ThrottlingManage from './throttling_dialog/throttling_manage';
+import TimeoutRetryManage from './timeout_retry_manage';
+import ThrottlingManage from './throttling_manage';
 
-    export default {
-      components: {
-        'timeout-retry-manage': TimeoutRetryManage,
-        'throttling-manage': ThrottlingManage,
-      },
-      data() {
-        return {
-          tabActive: 'timeout_retry',
-        };
-      },
+export default {
+  components: {
+    'timeout-retry-manage': TimeoutRetryManage,
+    'throttling-manage': ThrottlingManage,
+  },
+  data() {
+    return {
+      loading: false,
+      indexId: this.$route.query.indexId,
+      tabActive: 'timeout_retry',
+      templateList: [],
     };
+  },
+  methods: {
+    getTemplateList() {
+      this.loading = true;
+      this.$http.get(`/index_template/list.json?indexId=${this.indexId}`).then((data) => {
+        this.templateList = data.list;
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+    },
+  },
+  created() {
+    this.getTemplateList();
+  },
+};
 </script>
--- pallas-console-web/src/pages/index_detail/service_manage/timeout_retry_manage.vue
+++ pallas-console-web/src/pages/index_detail/service_manage/timeout_retry_manage.vue
@@ -8,13 +8,13 @@
         <div class="data-table-filter">
           <el-form :inline="true" class="demo-form-inline">
             <el-form-item label="超时时间(毫秒,需>=50ms)">
-              <el-input-number placeholder="超时时间(毫秒)" v-model="indexInfo.timeout" :min="0"></el-input-number>
+              <el-input-number placeholder="超时时间(毫秒)" v-model="indexConfigInfo.timeout" :min="0"></el-input-number>
             </el-form-item>
             <el-form-item label="重试次数(上限1次)">
-              <el-input-number placeholder="重试次数" v-model="indexInfo.retry" :min="0" :max="1"></el-input-number>
+              <el-input-number placeholder="重试次数" v-model="indexConfigInfo.retry" :min="0" :max="1"></el-input-number>
             </el-form-item>
             <el-form-item label="慢查询阈值(毫秒)">
-                <el-input-number placeholder="慢查询阈值(毫秒)" v-model="indexInfo.slowerThan" :min="0"></el-input-number>
+                <el-input-number placeholder="慢查询阈值(毫秒)" v-model="indexConfigInfo.slowerThan" :min="0"></el-input-number>
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="handleUpdate"><i class="fa fa-refresh"></i>更新</el-button>
@@ -24,23 +24,24 @@
       </el-collapse-item>
       <el-collapse-item title="模板级别配置" name="2">
         <template slot="title">
-            模板级别配置（若模板超时时间为0毫秒，则以当前索引的超时时间和重试为准。）
+            模板级别配置
+            <span style="color: #ddd">（若模板超时时间为0毫秒，则以当前索引的超时时间和重试为准。）</span>
         </template>
-        <el-table :data="templateList" border style="width: 100%" v-loading="loading" element-loading-text="请稍等···">
-          <el-table-column label="模板名称" min-width="380px">
+        <el-table :data="templateList" border style="width: 100%">
+          <el-table-column label="模板名称" show-overflow-tooltip>
               <template scope="scope">{{scope.row.templateName}}</template>
           </el-table-column>
-          <el-table-column label="超时时间(毫秒,需>=50ms)" width="380px">
+          <el-table-column label="超时时间(毫秒,需>=50ms)">
             <template scope="scope">
-              <el-input-number :placeholder="timeout" v-model="scope.row.timeout" :min="0"></el-input-number>
+              <el-input-number v-model="scope.row.timeout" :min="0"></el-input-number>
             </template>
           </el-table-column>
-          <el-table-column label="重试次数(上限1次)" width="380px">
+          <el-table-column label="重试次数(上限1次)">
             <template scope="scope">
-              <el-input-number :placeholder="retry" v-model="scope.row.retry" :min="0" :max="1"></el-input-number>
+              <el-input-number v-model="scope.row.retry" :min="0" :max="1"></el-input-number>
             </template>
           </el-table-column>
-          <el-table-column label="操作" width="370px">
+          <el-table-column label="操作" width="120px">
             <template scope="scope">
               <el-button type="primary" @click="handleTemplateUpdate(scope.row)"><i class="fa fa-refresh"></i>更新</el-button>
             </template>
@@ -53,28 +54,23 @@
 
 <script>
 export default {
+  props: ['templateList'],
   data() {
     return {
       loading: false,
       indexId: this.$route.query.indexId,
-      indexInfo: {},
-      isAllPrivilege: false,
-      templateInfo: {},
-      templateList: [],
-      temPanelHeight: {
-        height: document.body.clientHeight - 210,
-      },
+      indexConfigInfo: {},
       activeNames: ['1', '2'],
     };
   },
   methods: {
-    getIndexInfo() {
+    getIndexConfigInfo() {
       this.loading = true;
       this.$http.get(`/index/id.json?indexId=${this.indexId}`).then((data) => {
-        this.indexInfo = JSON.parse(JSON.stringify(data));
-        if (this.indexInfo.slowerThan == null) {
-          this.$set(this.indexInfo, 'slowerThan', '200');
-        }
+        this.indexConfigInfo = {
+          ...data,
+          slowerThan: data.slowerThan == null ? '200' : data.slowerThan,
+        };
       })
       .finally(() => {
         this.loading = false;
@@ -83,24 +79,18 @@ export default {
     handleUpdate() {
       const params = {
         indexId: this.indexId,
-        timeout: this.indexInfo.timeout,
-        retry: this.indexInfo.retry,
-        slowerThan: this.indexInfo.slowerThan,
+        timeout: this.indexConfigInfo.timeout,
+        retry: this.indexConfigInfo.retry,
+        slowerThan: this.indexConfigInfo.slowerThan,
       };
       this.loading = true;
       this.$http.post('/index/update/timeout_retry.json', params).then(() => {
-        this.$message.successMessage('更新成功');
+        this.$message.successMessage('索引配置更新成功', () => {
+          this.getIndexConfigInfo();
+        });
       })
       .finally(() => {
         this.loading = false;
-      });
-    },
-    handleCallback(data) {
-      this.templateList = data.list;
-    },
-    getTemplateList() {
-      return this.$http.get(`/index_template/list.json?indexId=${this.indexId}`).then((data) => {
-        this.handleCallback(data);
       });
     },
     handleTemplateUpdate(row) {
@@ -112,22 +102,17 @@ export default {
       };
       this.loading = true;
       this.$http.post('/index_template/update.json', params).then(() => {
-        this.$message.successMessage('更新成功');
+        this.$message.successMessage('模板配置更新成功', () => {
+          this.$emit('refresh-template');
+        });
       })
       .finally(() => {
         this.loading = false;
       });
     },
-    init() {
-      this.loading = true;
-      Promise.all([this.getTemplateList(), this.getClusters()]).then(() => {
-        this.loading = false;
-      });
-    },
   },
   created() {
-    this.getIndexInfo();
-    this.init();
+    this.getIndexConfigInfo();
   },
 };
 </script>
